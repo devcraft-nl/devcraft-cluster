c92d454e718181b51ead6d656c468f29a9134ab8
Dennis Kroesen <dkroesen@devcraft.nl>
2025-09-12 12:48:50 +0200

    adding grafana alloy with sops encryption

--- install-cloud.sh
+++ install-cloud.sh
@@ -4,4 +4,11 @@ helm install flux-operator oci://ghcr.io/controlplaneio-fluxcd/charts/flux-opera
   --namespace flux-system \
   --create-namespace
 
+export KEY_FP=4527EAAAEF676E9D3802CB489BCD8CD0C1B5A8CE
+
+gpg --export-secret-keys --armor "${KEY_FP}" |
+kubectl create secret generic sops-gpg \
+--namespace=flux-system \
+--from-file=sops.asc=/dev/stdin
+
 kubectl apply -f flux/flux-instance-prod.yaml
--- run-local.sh
+++ run-local.sh
@@ -8,4 +8,11 @@ helm install flux-operator oci://ghcr.io/controlplaneio-fluxcd/charts/flux-opera
   --namespace flux-system \
   --create-namespace
 
+export KEY_FP=C2C715BB173803D1A33C0B9E64D72306B5CEADB8
+
+gpg --export-secret-keys --armor "${KEY_FP}" |
+kubectl create secret generic sops-gpg \
+--namespace=flux-system \
+--from-file=sops.asc=/dev/stdin
+
 kubectl apply -f flux/flux-instance-local.yaml
